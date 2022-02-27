344b10f6858ca51559be35b3ec9c47749100a04f
Frederick <fzieber@gmail.com>
2022-02-27 09:58:53 -0800

    Lab7Tut2+Chal

---
+++ Lab7/Challenges/init-db.py
@@ -0,0 +1,38 @@
+import mysql.connector as mysql
+import os
+import datetime
+from dotenv import load_dotenv  # only required if using dotenv for creds
+
+# Store MySql credentials
+load_dotenv('credentials.env')
+db_host = os.environ['MYSQL_HOST']
+db_user = os.environ['MYSQL_USER']
+db_pass = os.environ['MYSQL_PASSWORD']
+
+
+
+# Connect to MySql
+db = mysql.connect(user=db_user, password=db_pass,
+                   host=db_host)
+cursor = db.cursor()
+
+# Create Midterm_db Database
+cursor.execute("CREATE DATABASE IF NOT EXISTS Plate_db;")
+cursor.execute("USE Plate_db")
+
+# Delete existing, then Create Data_Table table
+cursor.execute("DROP TABLE IF EXISTS Data_Table;")
+
+try:
+    cursor.execute("""
+    CREATE TABLE Data_Table (
+      name        VARCHAR(50) NOT NULL,
+      created_at  TIMESTAMP,
+      text        VARCHAR(50) NOT NULL 
+    );
+  """)
+except RuntimeError as err:
+    print("runtime error: {0}".format(err))
+
+db.commit()
+
